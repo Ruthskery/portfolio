9535400c4af63688bb7aa56800d9f5a2f3c24b98
LetNear <bopenales@my.cspc.edu.ph>
2025-02-08 11:55:50 +0800

    Second Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,30 +5,32 @@ import Nav from "@/Components/Nav";
 import Projects from "@/Components/Projects";
 import Services from "@/Components/Services";
 import Skills from "@/Components/Skills";
-import  React, { useState }  from "react";
+import React, { useState } from "react";
 
-const index = () => {
-
+// Rename the component from `index` to `Index`
+const Index = () => {
   const [nav, setNav] = useState(false);
 
   const openNav = () => setNav(true);
   const closeNav = () => setNav(false);
 
-  return <div className="overflow-x-hidden">
-    <div>
-      {/* Navigation */}
-      <Nav openNav={openNav} />
-      <MobileNav nav={nav} closeNav={closeNav} />
-      {/* Hero Section */}
-      <Hero/>
-      <div className="relative z-[30]">
-       <About/>
-       <Services />
-       <Skills />
-       <Projects />
+  return (
+    <div className="overflow-x-hidden">
+      <div>
+        {/* Navigation */}
+        <Nav openNav={openNav} />
+        <MobileNav nav={nav} closeNav={closeNav} />
+        {/* Hero Section */}
+        <Hero />
+        <div className="relative z-[30]">
+          <About />
+          <Services />
+          <Skills />
+          <Projects />
+        </div>
       </div>
     </div>
-    </div>
+  );
 };
 
-export default index;
+export default Index;
